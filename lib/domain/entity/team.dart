45d3e2199d8e81d25eb689504ddc6f8089d0a191
TiagoBruckmann <tiago_bruck@outlook.com.br>
2025-02-24 11:06:14 -0300

    feat: sort teams accordly to team score

--- lib/domain/entity/team.dart
+++ lib/domain/entity/team.dart
@@ -8,21 +8,33 @@ class TeamEntity extends Equatable {
   @PrimaryKey(autoGenerate: true)
   final int? id;
   final String name, league, logo;
+  final int score;
 
-  const TeamEntity( this.name, this.league, this.logo, { this.id });
+  const TeamEntity( this.name, this.league, this.logo, this.score, { this.id });
 
   factory TeamEntity.fromJson( dynamic json ) {
     return TeamEntity(
       json["name"],
       json["league"],
       json["logo"].toString().toLowerCase(),
+      json["score"],
     );
   }
 
-  @override
-  String toString() => "TeamEntity($name - $league)";
+  bool isEqual( TeamEntity team ) => team.id == id && team.logo == logo;
 
   @override
-  List<Object?> get props => [id, name, league, logo];
+  String toString() => "TeamEntity($name - $league - $score)";
+
+  @override
+  bool operator ==( Object other ) {
+    return other is TeamEntity && other.hashCode == hashCode;
+  }
+
+  @override
+  int get hashCode => Object.hash(name, league, logo, score);
+
+  @override
+  List<Object?> get props => [id, name, league, logo, score];
 
 }
